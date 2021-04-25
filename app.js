dda3517a76bdd7e2a9e1ae898f2b0aa7186d70fb
KietLNA <kietlna1997>
2021-04-25 11:59:18 +0700

    [ADD] swagger

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const app = express();
 const routes = require('./routes')
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000
+//swagger 
+var swaggerUi = require('swagger-ui-express')
+var fs = require('fs')
+var jsyaml = require('js-yaml');
+var spec = fs.readFileSync('swagger.yaml', 'utf8');
+var swaggerDocument = jsyaml.safeLoad(spec);
 
-    app.use('/',routes);
+
+app.use('/',routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
